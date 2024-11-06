d26801fc163ee1d58eb17944db004d9362a9b7e9
anthonylatorre764 <anthonylatorre764@gmail.com>
2024-11-06 02:43:22 -0500

    Added setInterval and clearInterval

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
 console.log("testing123")
 
 function normalCycle() {
-    document.getElementById("traffic_light").src="images/traffic_light_phases/green_arrow.png"
+    document.getElementById("traffic_light").src="images/traffic_light_phases/green_arrow.png";
 
     setTimeout(() => {
-        console.log("After 3 second delay");
         document.getElementById("traffic_light").src="images/traffic_light_phases/yellow_arrow.png";
     }, 3000);
 
@@ -19,4 +18,17 @@ function normalCycle() {
     setTimeout(() => {
         document.getElementById("traffic_light").src="images/traffic_light_phases/red_light.png";
     }, 12000);
+}
+
+
+
+
+function startCycle() {
+    normalCycle();
+    const intervalID = setInterval(normalCycle, 15000);                         // passes a reference to the function normalCycle (instead of calling it immediately)
+}
+
+// Find a way to end the light cycle started by ^^ startCycle
+function endCycle() {
+    clearInterval(intervalID);
 }
